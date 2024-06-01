3532ab5bc0d43b550075090542b931f81689a9a2
Akshay <pawarakshay13@gmail.com>
2024-06-01 18:19:02 +0530

    header changed

---
+++ compose/ui_pages/page_header.js
@@ -0,0 +1,13 @@
+ document.write(`
+    <header>
+        <h1 class="header-heading">Jetpack Compose Study</h1>
+        <p>Note: 💻🖥️✅ | 📱❎</p>
+        <div>
+            <a class="menu" href="download.html" id="downloadLink">Download</a>
+            <a class="menu" href="compose/ui_pages/todo.html" id="todoLink">To-Do</a>
+            <a class="menu" href="compose/ui_pages/notes.html" id="notesLink">Notes</a>
+            <a class="menu selected" href="index.html" id="composeLink">Compose</a>
+            <a class="menu" href="profile/index.html" id="profileLink">Profile</a>
+        </div>
+    </header>
+    `);
